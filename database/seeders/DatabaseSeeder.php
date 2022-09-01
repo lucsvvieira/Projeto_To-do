d498e1d0993e6217cee7780150711646d916f12a
Lucas Vieira <vieira07lucas@gmail.com>
2022-09-01 14:36:10 -0300

    Introducao as Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,9 +19,10 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             UserSeeder::class,
-            CategorySeeder::class,
-            TaskSeeder::class
+            // TaskSeeder::class
         ]);
+
+        Category::factory(5)->create();
 
         // \App\Models\User::factory(10)->create();
 
