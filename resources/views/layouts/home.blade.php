093fbdb01218e6fd4b3267ba3a1429258ff83649
Lucas Vieira <vieira07lucas@gmail.com>
2022-09-12 11:43:35 -0300

    Criando estrutura básica do template

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -43,6 +43,21 @@
                             <option value="1"> Todas as tarefas </option>
                         </select>
                     </div>
+                    <div class="task_list">
+                        <div class="task">
+                            <div class="title">
+                                <input type="checkbox">
+                                <div class="task_title">Título da tarefa</div>
+                            </div>
+                            <div class="priority">
+                                <div class="sphere"> </div>
+                                <div>Título da tarefa</div>
+                            </div>
+                            <div class="actions">
+                                Editar - Excluir
+                            </div>
+                        </div>
+                    </div>
                 </section>
             </main>
         </div>
