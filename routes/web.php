3dbe5c6802687f0f804aeb9cd0d6c40efdc71c5d
Lucas Vieira <vieira07lucas@gmail.com>
2022-10-17 13:48:58 -0300

    Criando a Navegação

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/task/new', [TaskController::class, 'create'])->name('task.create');
+Route::get('/task', [TaskController::class, 'create'])->name('task.create');
+
+Route::get('/login', [AuthController::class, 'index'])->name('login');
+Route::get('/register', [AuthController::class, 'register'])->name('register');
 
 Route::get('/', function () {
     return view('home');
