4de24e528e7ef677cdcf4bb73f7724f257462341
Lucas Vieira <vieira07lucas@gmail.com>
2022-10-19 11:07:13 -0300

    Finalização da configuração da navegação

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,4 +7,24 @@ use Illuminate\Http\Request;
 class TaskController extends Controller
 {
     //
+    public function index() {
+
+    }
+
+    public function create(Request $request) {
+        
+        return view('tasks.create');
+        
+    }
+
+    public function edit(Request $request) {
+        
+        return view('tasks.edit');
+
+    }
+
+    public function delete(Request $request) {
+        // Deleta e redireciona de volta para a Home
+        return redirect(route('home'));
+    }
 }
