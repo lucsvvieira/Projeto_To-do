0f3a42347cbbb9944baef2ed9d16cc804cdb7495
Lucas Vieira <vieira07lucas@gmail.com>
2022-10-20 17:07:45 -0300

    Validação do formulário

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,7 +16,24 @@ class AuthController extends Controller
     }
 
     public function register_action(Request $request) {
-        
+        /*
+            ******************************************
+            Regras para registro de usuários
+
+            - O usuário tem que ter um nome
+            - O email tem que ser único na tabela users
+            - Todos os campos são REQUIRED
+            - Password tem que possuir no mínimo 6 caracteres
+            
+            ******************************************
+        */
+
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6'
+        ]);
+
         $data = $request->only('name', 'email', 'password');
         $userCreated = User::create($data);
         dd($userCreated);
