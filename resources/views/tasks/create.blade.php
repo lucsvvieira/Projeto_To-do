49e8f5a1556d1816b443bd9457f592c4c47a4f77
Lucas Vieira <vieira07lucas@gmail.com>
2022-10-20 12:18:03 -0300

    Finalizando a componentização do formulário

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,40 +1,28 @@
 <x-layout page="B7Web Todo: Login">
     <x-slot:btn>
-        <a href="{{route('home')}}" class="btn btn-primary"> 
-            Voltar 
+        <a href="{{ route('home') }}" class="btn btn-primary">
+            Voltar
         </a>
     </x-slot:btn>
 
     <section id="create_task_section">
         <h1>Criar Tarefa</h1>
         <form action="">
-            <x-form.text_input name="title" label="Título da Task" placeholder="Digite o título da sua task" required="required" />
-            <x-form.text_input type="date" name="due_date" label="Data de Realização" required="required" />
-            <div class="inputArea">
-                <label for="category">
-                    Categoria
-                </label>
-                <select id="category" name="category" required>
-                    <option selected disabled value="">
-                        Selecione a categoria
-                    </option>
-                    <option value="">
-                        Valor qualquer
-                    </option>
-                </select>
-            </div>
-            <div class="inputArea">
-                <label for="title">
-                    Descrição da tarefa
-                </label>
-                <textarea name="description" placeholder="Digite uma descrição para sua tarefa"></textarea>
-            </div>
 
-            <div class="inputArea">
-                <button type="submit" class="btn btn-primary">
-                    Criar Tarefa
-                </button>
-            </div>
+            <x-form.text_input name="title" label="Título da Task" placeholder="Digite o título da sua task" required="required" />
+
+            <x-form.text_input type="date" name="due_date" label="Data de Realização" required="required" />
+
+            <x-form.select_input name="category" label="Categoria" placeholder="Digite o título da sua task">
+                <option>Valor qualquer</option>
+            </x-form.select_input>
+
+            <x-form.textarea_input 
+                name="description" 
+                placeholder="Digite a descrição da tarefa"
+            />
+
+            <x-form.form_button resetTxt="Resetar" submitTxt="Criar uma Tarefa" />
 
         </form>
     </section>
